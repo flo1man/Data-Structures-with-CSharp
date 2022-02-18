765da775d3472e7ff12ce7386b473f1397f136c2
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-02-18 20:06:51 +0200

    Initial commit

---
+++ 03. Trees and Traversal (BFS, DFS)/Tree/IAbstractTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    public interface IAbstractTree<T>
+    {
+        T Value { get; }
+        Tree<T> Parent { get; }
+        IReadOnlyCollection<Tree<T>> Children { get; }
+
+        ICollection<T> OrderBfs();
+
+        ICollection<T> OrderDfs();
+
+        void AddChild(T parentKey, Tree<T> child);
+
+        void RemoveNode(T nodeKey);
+
+        void Swap(T firstKey, T secondKey);
+    }
+}
---
+++ 03. Trees and Traversal (BFS, DFS)/Tree/Tree.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tree
+{
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> children;
+
+        public Tree(T value)
+        {
+            this.Value = value;
+            this.Parent = null;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            foreach (Tree<T> child in children)
+            {
+                child.Parent = this;
+                this.children.Add(child);
+            }
+        }
+
+        public T Value { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+        public IReadOnlyCollection<Tree<T>> Children => children.AsReadOnly();
+
+        public bool IsRootDeleted { get; private set; }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            var searchedNode = this.FindBfs(parentKey);
+
+            CheckEmptyTree(searchedNode);
+
+            searchedNode.children.Add(child);
+        }
+
+        
+
+        public ICollection<T> OrderBfs()
+        {
+            List<T> result = new List<T>();
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+
+            if (IsRootDeleted)
+            {
+                return result;
+            }
+
+            result.Add(this.Value);
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+                
+                foreach (var child in subtree.Children)
+                {
+                    queue.Enqueue(child);
+                    result.Add(child.Value);
+                }
+            }
+
+            return result;
+        }
+
+        public ICollection<T> OrderDfs()
+        {
+            List<T> result = new List<T>();
+
+            if (IsRootDeleted)
+            {
+                return result;
+            }
+
+            this.Dfs(this, result);
+
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            var searchedNode = this.FindBfs(nodeKey);
+
+            CheckEmptyTree(searchedNode);
+
+            foreach (var child in searchedNode.Children)
+            {
+                child.Parent = null;
+            }
+
+            searchedNode.children.Clear();
+            var searchedParent = searchedNode.Parent;
+
+            if (searchedParent == null)
+            {
+                this.IsRootDeleted = true;
+            }
+            else
+            {
+                searchedParent.children.Remove(searchedNode);
+            }
+
+            searchedNode.Value = default(T);
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            var firstNode = FindBfs(firstKey);
+            var secondNode = FindBfs(secondKey);
+            CheckEmptyTree(firstNode);
+            CheckEmptyTree(secondNode);
+
+            var firstParent = firstNode.Parent;
+            var secondParent = secondNode.Parent;
+
+            if (firstParent == null)
+            {
+                SwapRoot(secondNode);
+                return;
+            }
+
+            if (secondParent == null)
+            {
+                SwapRoot(firstNode);
+                return;
+            }
+
+            firstNode.Parent = secondParent;
+            secondNode.Parent = firstParent;
+
+            int indexOfFirst = firstParent.children.IndexOf(firstNode);
+            int indexOfSecond = secondParent.children.IndexOf(secondNode);
+
+            firstParent.children[indexOfFirst] = secondNode;
+            secondParent.children[indexOfSecond] = firstNode;
+        }
+
+        private void Dfs(Tree<T> tree, List<T> list)
+        {
+
+            foreach (var item in tree.Children)
+            {
+                Dfs(item, list);
+            }
+
+            list.Add(tree.Value);
+        }
+
+        private Tree<T> FindBfs(T parentKey)
+        {
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+
+            queue.Enqueue(this);
+
+            while (queue.Count > 0)
+            {
+                var subtree = queue.Dequeue();
+
+                if (subtree.Value.Equals(parentKey))
+                {
+                    return subtree;
+                }
+
+                foreach (var child in subtree.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private void CheckEmptyTree(Tree<T> tree)
+        {
+            if (tree == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        private void SwapRoot(Tree<T> secondNode)
+        {
+            this.Value = secondNode.Value;
+            this.children.Clear();
+
+            foreach (var child in secondNode.Children)
+            {
+                this.children.Add(child);
+            }
+        }
+    }
+}
